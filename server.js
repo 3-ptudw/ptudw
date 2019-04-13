36ce3be3e97b9daf46daabebd452f94b5c1d0cb8
ntuan2502 <44097595+ntuan2502@users.noreply.github.com>
2019-04-13 15:00:47 +0700

    first-commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static('public'))
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.listen(process.env.PORT || 3000);
+
+app.get("/", function (request, response) {
+    response.render("index");
+});
+
+app.get("/1", function (request, response) {
+    response.render("index1");
+});
