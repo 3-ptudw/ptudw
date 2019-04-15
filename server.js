5b248f7f23810fa4929d6a8b926ddad7cf4f01ca
Tuan Nguyen <tuanng@pixiostudio.com>
2019-04-15 12:54:46 +0700

    create admin/login

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static('public'))
+app.use('/static', express.static('public'));
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
 app.listen(process.env.PORT || 3000);
 
-app.get("/", function (request, response) {
-    response.render("index");
-});
-
-app.get("/1", function (request, response) {
-    response.render("index1");
+app.get('*', function(req, res){
+   res.send('Sorry, this is an invalid URL.');
 });
