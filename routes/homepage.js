984ade9107dc3aba3a0f98e3b3add5b97b5d2a81
ntuan2502 <44097595+ntuan2502@users.noreply.github.com>
2019-04-20 18:27:43 +0700

    homepage template

---
+++ routes/homepage.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('homepage/index', { page: 'Home', menuId: 'home' });
+});
+
+/* GET homepage phim ảnh page. */
+router.get('/phim-anh', function(req, res, next) {
+    res.render('homepage/phim-anh', { page: 'Phim ảnh', menuId: 'Phim ảnh' });
+});
+
+/* GET homepage công nghệ page. */
+router.get('/cong-nghe', function(req, res, next) {
+    res.render('homepage/cong-nghe', { page: 'Công nghệ', menuId: 'Công nghệ' });
+});
+
+/* GET homepage Xuất bản' page. */
+router.get('/xuat-ban', function(req, res, next) {
+    res.render('homepage/xuat-ban', { page: 'Xuất bản', menuId: 'Xuất bản' });
+});
+
+/* GET page những câu hỏi lớn Avengers Endgame phải trả lời. */
+router.get('/nhung-cau-hoi-lon-avengers-endgame-phai-tra-loi', function(req, res, next) {
+    res.render('homepage/nhung-cau-hoi-lon-avengers-endgame-phai-tra-loi', { page: 'Những câu hỏi lớn "Avengers: Endgame" phải trả lời', menuId: 'Những câu hỏi lớn "Avengers: Endgame" phải trả lời' });
+});
+
+/* GET page Chi tiết TV QLED 8K đầu tiên của Samsung ở Việt Nam. */
+router.get('/chi-tiet-tv-qled-8k-dau-tien-cua-samsung-o-viet-nam', function(req, res, next) {
+    res.render('homepage/chi-tiet-tv-qled-8k-dau-tien-cua-samsung-o-viet-nam', { page: 'Chi tiết TV QLED 8K đầu tiên của Samsung ở Việt Nam', menuId: 'Chi tiết TV QLED 8K đầu tiên của Samsung ở Việt Nam' });
+});
+
+/* GET page Khai mạc Ngày sách Việt Nam tại TP.HCM. */
+router.get('/khai-mac-ngay-sach-viet-nam-tai-tphcm', function(req, res, next) {
+    res.render('homepage/khai-mac-ngay-sach-viet-nam-tai-tphcm', { page: 'Khai mạc Ngày sách Việt Nam tại TP.HCM', menuId: 'Khai mạc Ngày sách Việt Nam tại TP.HCM' });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { page: 'Home', menuId: 'home' });
-});
-
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-    res.render('about', { page: 'About', menuId: 'about' });
-});
-
-/* GET contact page. */
-router.get('/contact', function(req, res, next) {
-    res.render('contact', { page: 'Contact', menuId: 'contact' });
+    res.redirect('homepage');
 });
 
 module.exports = router;
