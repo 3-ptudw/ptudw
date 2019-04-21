ee605e043e8f5d8f6dbf9dccf64705ac9f5f1dc9
ntuan2502 <44097595+ntuan2502@users.noreply.github.com>
2019-04-21 13:02:03 +0700

    config heroku

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+
+var app = express();
+app.listen(process.env.PORT || 3000);
+
+// view engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 var homepageRouter = require('./routes/homepage');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET admin listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    res.redirect('login');
 });
 
 /* GET admin login page. */
