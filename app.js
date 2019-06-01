f734dae1509fbbf4176d4c73b84209d1500fd377
Tuan Nguyen <tuanng@pixiostudio.com>
2019-06-02 01:49:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ app.use((error, req, res, next) => {
     })
 })
 
-app.listen(process.env.port || 3000, () => {
-    console.log('server is running at http://localhost:3000');
-})
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
+});
